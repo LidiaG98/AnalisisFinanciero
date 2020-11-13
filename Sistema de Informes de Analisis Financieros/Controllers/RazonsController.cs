6ea7b11ce257bd51ce9a63e0aebbeda6be8bcd22
Corcel Indomable <fermerinonew@gmail.com>
2020-11-13 01:17:31 -0600

    Mathosparser INdex Razon cards ratios lo mas seguro esuq enada sirva

--- Sistema de Informes de Analisis Financieros/Controllers/RazonsController.cs
+++ Sistema de Informes de Analisis Financieros/Controllers/RazonsController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Sistema_de_Informes_de_Analisis_Financieros.Models;
 
@@ -20,7 +19,105 @@ namespace Sistema_de_Informes_de_Analisis_Financieros
 
         // GET: Razons
         public async Task<IActionResult> Index()
-        {
+        { // Convierte texto en funciones y puede mostrar el resultado
+            Mathos.Parser.MathParser parse = new Mathos.Parser.MathParser();
+            
+            //contextos que creí necesarios para seguir v,:
+            var cliente = from s in _context.Empresa select s;
+            var catCuent = from e in _context.Catalogodecuenta select e;
+            var cuenta = from x in _context.Cuenta select x;
+            var empresa = from y in _context.Empresa select y;
+            var razon = from m in _context.Razon select m;
+            var tipocuenta = from l in _context.Tipocuenta select l;
+            var balance = from n in _context.Valoresdebalance select n;
+            //Lista que guarda los resultados de los ratios
+            List<double> Resultado = new List<double>();
+            //delimitador generalizado para recuperar el nombre de las cuentas
+            // string delimitador = "";
+
+           //recupero el usuario que está logeado, asumo explota si no hay nadie logeado
+            var user = this.User;
+            List<Usuario> u = _context.Users.Include(e => e.Idempresa).Where(e => e.UserName == user.Identity.Name).ToList();
+            List<Cuenta> cuex = new List<Cuenta>();
+           //recupero el catalogo de la empresa del usuario logeado
+            catCuent = catCuent.Where(y => y.Idempresa == u[0].Idempresa.Idempresa);
+           /* foreach(Catalogodecuenta cato in catCuent)
+            {
+                cuex.Add(cuenta.FirstOrDefault(x => x.Idcuenta == cato.Idcuenta));
+                
+            }*/
+           //necesitaba tener las cuentas del catalogo que filtré arriba, y no sa{bia como xd tons hice un join 
+           //entre la tabla
+            var cuentaCat = _context.Cuenta.Join(_context.Catalogodecuenta,
+                cuentax => cuentax.Idcuenta,
+                catcuentx => catcuentx.Idcuenta,
+                (cuentax, catcuentx) => new
+                {
+                    cuentaID = cuentax.Idcuenta,
+                    Idtipocuenta = cuentax.Idtipocuenta
+
+
+                }) ;
+            foreach (Razon rakata in razon)
+            {
+               string[] razoncitanum= rakata.numerador.Split('+','-','*');
+               string[] razoncitaden = rakata.numerador.Split('+', '-', '*');
+                List<Tipocuenta> tipoC = new List<Tipocuenta>();
+                List<int> cuentax = new List<int>();
+                List<double> suma = new List<double>();
+                
+               
+
+                //pasar a numero el numerador de los ratios
+                for(int i=0;i< razoncitanum.Length;i++)
+                {
+                    var tipoCC= tipocuenta.FirstOrDefault(l => l.Nomtipocuenta.Equals(razoncitanum[i]));
+                    if (tipoCC != null)
+                    {
+
+
+                        var cuentaX = cuentaCat.Where(x => x.Idtipocuenta == tipoCC.Idtipocuenta);
+                        foreach (var c in cuentaX)
+                        {
+                            var banlancin = balance.FirstOrDefault(n => n.Idcuenta == c.cuentaID);
+                            //sumo el total de las cuentas del mismo tipo
+                            suma[i] = suma[i] + banlancin.Valorcuenta;
+                        }
+                        rakata.numerador = rakata.numerador.Replace(razoncitanum[i], suma[i].ToString());
+                         
+
+                    }
+                   
+                }
+                for (int i = 0; i < razoncitaden.Length; i++)
+                {
+                    var tipoCC = tipocuenta.FirstOrDefault(l => l.Nomtipocuenta.Equals(razoncitaden[i]));
+                    var cuentaX = cuenta.Where(x => x.Idtipocuenta == tipoCC.Idtipocuenta);
+                    foreach (Cuenta c in cuentaX)
+                    {
+                        var banlancin = balance.FirstOrDefault(n => n.Idcuenta == c.Idcuenta);
+                        suma[i] = suma[i] + banlancin.Valorcuenta;
+                    }
+                    rakata.denominador = rakata.denominador.Replace(razoncitanum[i], suma[i].ToString());
+                }
+
+
+
+                
+                Resultado.Add(( parse.Parse(rakata.numerador + "/" + rakata.denominador )));
+                
+            }
+            ViewData["Resultados"] = Resultado;
+
+           
+                
+                 
+
+
+            // var empresa = empresa.Where(s => s.clienteID.Contains(Buscar) || s.nombre.Contains(Buscar) || s.apellido.Contains(Buscar));
+
+
+
             return View(await _context.Razon.ToListAsync());
         }
 
---
+++ Sistema de Informes de Analisis Financieros/ViewModels/AnalisisHorizontalViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Informes_de_Analisis_Financieros.ViewModels
+{
+    public class AnalisisHorizontalViewModel
+    {
+    }
+}
